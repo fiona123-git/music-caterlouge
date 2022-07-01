99d6729c1dd6c0607658cd2d3d12697b65027f58
fiona <fibsy79@yahoo.com>
2022-07-01 09:10:49 +0200

    added all

--- itunes/server/server.js
+++ itunes/server/server.js
@@ -7,6 +7,16 @@ const helmet = require("helmet");
 
 app.use(helmet());
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname,
+            'frontend', 'build', 'index.html'));
+    });
+    
+    app.use(express.static(path.join(__dirname, 'frontend/build')))
+}
+
 
 app.get('/api/:searchTerm/:mediaType', (req, res) => {
     let searchTerm = req.params.searchTerm
